3eb9c18d14fddda103a5ce8245da8c0f72311ba0
Jérémy Bonhomme <jeremy.bonhomme2@gmail.com>
2017-11-26 22:42:53 +0100

    last commit

--- src/app/main/module.ts
+++ src/app/main/module.ts
@@ -2,7 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MenuC } from '../menu/handler';
+import { MenuC } from '../menu/component';
 import { appRouting } from '../root/routing';
 import { UserMD } from '../users/module';
 import { AppC } from './component';
